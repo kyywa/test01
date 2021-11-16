2f766227f63536203288d42fdfd2359d9b5f8a98
Tanuló 301-18 <301-18@KKSZKI.LOCAL>
2021-11-16 18:04:07 +0100

    2/2 kész 2 megoldással

--- whatever.py
+++ whatever.py
@@ -10,9 +10,20 @@ def average(tlist):
         tmp += tlist[i]
     return float(tmp/len(tlist))
 
+def min_val(tlist):
+    tmp = ""
+    for i in range(0,len(tlist)-1):
+        if tmp == "":
+            tmp = tlist[i]
+        elif tmp>tlist[i]:
+            tmp =  tlist[i] 
+    return tmp
+
 
 numberlist = []
 for i in range(0,10):
     numberlist.append(random.randint(0,100))
 print(average(numberlist))
+print(min_val(numberlist))
+print(min(numberlist))
 
