b1f353ef247e46da245add947d912b66a0b9686d
Tanuló 301-18 <301-18@KKSZKI.LOCAL>
2021-11-16 17:51:43 +0100

    2/1 kész

--- whatever.py
+++ whatever.py
@@ -1,3 +1,18 @@
+import random
+
 #print("proba1")
 #print("proba2")
 print("Az összeg: "+ str(int(input("Az első szám:"))+(int(input("A második szám:")))))
+
+def average(tlist):
+    tmp = 0
+    for i in range(0,len(tlist)-1):
+        tmp += tlist[i]
+    return float(tmp/len(tlist))
+
+
+numberlist = []
+for i in range(0,10):
+    numberlist.append(random.randint(0,100))
+print(average(numberlist))
+
